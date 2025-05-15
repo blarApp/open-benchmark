9f2f17aec2f07d9e6172db6c07f8efb12f10e21f
v4rgas <66626747+v4rgas@users.noreply.github.com>
2025-05-15 17:37:49 -0400

    d476865d-a7cf-4f85-b1bd-0a04cd4a9caa

---
+++ thefuck_9/tests/functional/test_tcsh.py
@@ -0,0 +1,52 @@
+import pytest
+from tests.functional.plots import with_confirmation, without_confirmation, \
+    refuse_with_confirmation, select_command_with_arrows
+
+containers = (('thefuck/ubuntu-python3-tcsh',
+               u'''FROM ubuntu:latest
+                   RUN apt-get update
+                   RUN apt-get install -yy python3 python3-pip python3-dev git
+                   RUN pip3 install -U setuptools
+                   RUN ln -s /usr/bin/pip3 /usr/bin/pip
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'),
+              ('thefuck/ubuntu-python2-tcsh',
+               u'''FROM ubuntu:latest
+                   RUN apt-get update
+                   RUN apt-get install -yy python python-pip python-dev git
+                   RUN pip2 install -U pip setuptools
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'))
+
+
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'tcsh')
+    proc.sendline(u'setenv PYTHONIOENCODING utf8')
+    proc.sendline(u'eval `thefuck --alias`')
+    return proc
+
+
+@pytest.mark.functional
+def test_with_confirmation(proc, TIMEOUT):
+    with_confirmation(proc, TIMEOUT)
+
+
+@pytest.mark.functional
+def test_select_command_with_arrows(proc, TIMEOUT):
+    select_command_with_arrows(proc, TIMEOUT)
+
+
+@pytest.mark.functional
+def test_refuse_with_confirmation(proc, TIMEOUT):
+    refuse_with_confirmation(proc, TIMEOUT)
+
+
+@pytest.mark.functional
+def test_without_confirmation(proc, TIMEOUT):
+    without_confirmation(proc, TIMEOUT)
+
+# TODO: ensure that history changes.
