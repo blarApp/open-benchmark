e0d1a72402d27aa2245b488f8cd227b804c88761
v4rgas <66626747+v4rgas@users.noreply.github.com>
2025-05-15 16:47:50 -0400

    24b207a7-c286-454f-a866-c5818db987da

--- thefuck_1/tests/rules/test_pip_unknown_command.py
+++ thefuck_1/tests/rules/test_pip_unknown_command.py
@@ -4,23 +4,13 @@ from thefuck.types import Command
 
 
 @pytest.fixture
+def pip_unknown_cmd():
+    return '''ERROR: unknown command "instatl" - maybe you meant "install"'''
+
+
+@pytest.fixture
 def pip_unknown_cmd_without_recommend():
     return '''ERROR: unknown command "i"'''
-
-
-@pytest.fixture
-def broken():
-    return 'instatl'
-
-
-@pytest.fixture
-def suggested():
-    return 'install'
-
-
-@pytest.fixture
-def pip_unknown_cmd(broken, suggested):
-    return 'ERROR: unknown command "{}" - maybe you meant "{}"'.format(broken, suggested)
 
 
 def test_match(pip_unknown_cmd, pip_unknown_cmd_without_recommend):
@@ -29,9 +19,6 @@ def test_match(pip_unknown_cmd, pip_unknown_cmd_without_recommend):
                              pip_unknown_cmd_without_recommend))
 
 
-@pytest.mark.parametrize('script, broken, suggested, new_cmd', [
-    ('pip un+install thefuck', 'un+install', 'uninstall', 'pip uninstall thefuck'),
-    ('pip instatl', 'instatl', 'install', 'pip install')])
-def test_get_new_command(script, new_cmd, pip_unknown_cmd):
-    assert get_new_command(Command(script,
-                                   pip_unknown_cmd)) == new_cmd
+def test_get_new_command(pip_unknown_cmd):
+    assert get_new_command(Command('pip instatl',
+                                   pip_unknown_cmd)) == 'pip install'
