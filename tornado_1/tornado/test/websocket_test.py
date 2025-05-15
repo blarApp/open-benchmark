c16b17a56074f13fd50d692e5668618ab290a82e
v4rgas <66626747+v4rgas@users.noreply.github.com>
2025-05-15 15:02:24 -0400

    354ef688-58b0-4cfa-b422-151f60eaa363

--- tornado_1/tornado/test/websocket_test.py
+++ tornado_1/tornado/test/websocket_test.py
@@ -198,12 +198,6 @@ class ErrorInAsyncOpenHandler(TestWebSocketHandler):
         raise Exception("boom")
 
 
-class NoDelayHandler(TestWebSocketHandler):
-    def open(self):
-        self.set_nodelay(True)
-        self.write_message("hello")
-
-
 class WebSocketBaseTestCase(AsyncHTTPTestCase):
     @gen.coroutine
     def ws_connect(self, path, **kwargs):
@@ -264,7 +258,6 @@ class WebSocketTest(WebSocketBaseTestCase):
                 ),
                 ("/error_in_open", ErrorInOpenHandler),
                 ("/error_in_async_open", ErrorInAsyncOpenHandler),
-                ("/nodelay", NoDelayHandler),
             ],
             template_loader=DictLoader({"message.html": "<b>{{ message }}</b>"}),
         )
@@ -568,12 +561,6 @@ class WebSocketTest(WebSocketBaseTestCase):
             ws = yield self.ws_connect("/error_in_async_open")
             res = yield ws.read_message()
         self.assertIsNone(res)
-
-    @gen_test
-    def test_nodelay(self):
-        ws = yield self.ws_connect("/nodelay")
-        res = yield ws.read_message()
-        self.assertEqual(res, "hello")
 
 
 class NativeCoroutineOnMessageHandler(TestWebSocketHandler):
